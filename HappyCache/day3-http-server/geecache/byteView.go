b9370fcf26b7d5cbb7751f7c5d47d6752399c701
HappyBinbin <4613913312qq.com>
2024-06-20 16:20:35 +0800

    [ADD] HTTP Server and consistentHash

---
+++ HappyCache/day3-http-server/geecache/byteView.go
@@ -0,0 +1,26 @@
+package geecache
+
+type ByteView struct {
+	v []byte
+}
+
+func (b ByteView) Len() int {
+	return len(b.v)
+}
+
+func (b*ByteView) ByteSlice() []byte {
+	return cloneBytes(b.v)
+}
+
+func cloneBytes(b []byte) []byte {
+	c := make([]byte, len(b))
+	copy(c, b)
+	return c
+}
+
+func (b ByteView) String() string {
+	return string(b.v)
+}
+
+
+
---
+++ HappyCache/day4-consistenthash/go.mod
@@ -0,0 +1,7 @@
+module GeeHappy
+
+go 1.17
+
+require geecache v0.0.0
+
+replace geecache => ./geecache
